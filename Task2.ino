4dbb811c03b3642dff65ba796454c466dccc31c4
n-lavash <79999380+n-lavash@users.noreply.github.com>
2023-04-07 16:54:45 +0400

    Update Task2.ino

--- Task2.ino
+++ Task2.ino
@@ -2,80 +2,93 @@
 #include "TaskManagerIO.h"
 
 
-int ledState1 = LOW;
+const int pin1 = 3;  
+const int pin2 = 5;  
+const int pin3 = 6;
+const int pin4 = 9;
+const int pin5 = 10;
+const int pin6 = 11;
+
+int ledState1 = LOW;  
 int ledState2 = LOW;
 int ledState3 = LOW;
 int ledState4 = LOW;
 int ledState5 = LOW;
 int ledState6 = LOW;
 
+const long intervalMls1 = 100;
+const long intervalMls2 = 600;
+const long intervalMls3 = 7000;
+const long intervalMls4 = 15000;
+const long intervalMls5 = 80000;
+const long intervalMls6 = 200000;
+
 void setup() {
-    Serial.begin(115200);
     
-    pinMode(3, OUTPUT);
-    pinMode(5, OUTPUT);
-    pinMode(6, OUTPUT);
-    pinMode(9, OUTPUT);
-    pinMode(10, OUTPUT);
-    pinMode(11, OUTPUT);
+    pinMode(pin1, OUTPUT);
+    pinMode(pin2, OUTPUT);
+    pinMode(pin3, OUTPUT);
+    pinMode(pin4, OUTPUT);
+    pinMode(pin5, OUTPUT);
+    pinMode(pin6, OUTPUT);
     
-    taskManager.scheduleFixedRate(30, [] {
+    taskManager.scheduleFixedRate(intervalMls1, [] {
       if (ledState1 == LOW) {
         ledState1 = HIGH;
-        digitalWrite(3, HIGH);
+        digitalWrite(pin1, ledState1);
       } else {
         ledState1 = LOW;
-        digitalWrite(3, LOW);
+        digitalWrite(pin1, ledState1);
       }
     }, TIME_MICROS);
 
-    taskManager.scheduleFixedRate(400, [] {
+    taskManager.scheduleFixedRate(intervalMls2, [] {
       if (ledState2 == LOW) {
         ledState2 = HIGH;
-        digitalWrite(5, HIGH);
+        digitalWrite(pin2, ledState2);
       } else {
         ledState2 = LOW;
-        digitalWrite(5, LOW);
+        digitalWrite(pin2, ledState2);
       }
     }, TIME_MICROS);
 
-    taskManager.scheduleFixedRate(5000, [] {
+    taskManager.scheduleFixedRate(intervalMls3, [] {
       if (ledState3 == LOW) {
         ledState3 = HIGH;
-        digitalWrite(6, HIGH);
+        digitalWrite(pin3, ledState3);
       } else {
         ledState3 = LOW;
-        digitalWrite(6, LOW);
+        digitalWrite(pin3, ledState3);
       }
     }, TIME_MICROS);
 
-    taskManager.scheduleFixedRate(10000, [] {
+    taskManager.scheduleFixedRate(intervalMls4, [] {
       if (ledState4 == LOW) {
         ledState4 = HIGH;
-        digitalWrite(9, HIGH);
+        digitalWrite(pin4, ledState4);
       } else {
         ledState4 = LOW;
-        digitalWrite(9, LOW);
+        digitalWrite(pin4, ledState4);
       }
     }, TIME_MICROS);
 
-    taskManager.scheduleFixedRate(40000, [] {
+    taskManager.scheduleFixedRate(intervalMls5, [] {
       if (ledState5 == LOW) {
         ledState5 = HIGH;
-        digitalWrite(10, HIGH);
+        digitalWrite(pin5, ledState5);
       } else {
         ledState5 = LOW;
-        digitalWrite(10, LOW);
+        digitalWrite(pin5, ledState5);
       }
     }, TIME_MICROS);
 
-    taskManager.scheduleFixedRate(100000, [] {
+    taskManager.scheduleFixedRate(intervalMls6, [] {
       if (ledState6 == LOW) {
         ledState6 = HIGH;
-        digitalWrite(11, HIGH);
+        digitalWrite(pin6, ledState6);
       } else {
         ledState6 = LOW;
-        digitalWrite(11, LOW);
+        digitalWrite(pin6, ledState6);
       }
     }, TIME_MICROS);
     
